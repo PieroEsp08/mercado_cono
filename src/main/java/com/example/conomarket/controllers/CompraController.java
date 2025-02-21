a80d48b5e3fe4d44d5fd2ca45211c47d96624f17
PieroEsp08 <piero_795@hotmail.com>
2025-02-20 21:24:05 -0500

    commit CORS

--- src/main/java/com/example/conomarket/controllers/CompraController.java
+++ src/main/java/com/example/conomarket/controllers/CompraController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/compras")
+@CrossOrigin(origins = "*")
 public class CompraController {
 
     @Autowired
--- src/main/java/com/example/conomarket/controllers/ProductoController.java
+++ src/main/java/com/example/conomarket/controllers/ProductoController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/productos")
+@CrossOrigin(origins = "*")
 public class ProductoController {
 
     @Autowired
